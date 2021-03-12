a6290982bf1e7f9b3a168e846c4e704157d01203
Ping <4ivrik@ukr.net>
2021-03-12 19:35:08 +0200

    Updates

--- precache-manifest.a7af3a55ed10ea1891c224ae70d1a412.js
+++ precache-manifest.a7af3a55ed10ea1891c224ae70d1a412.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "06e2dfd146c1e4e3b3aabdae8913239b",
+    "revision": "4af92f81932959e82880cd4ecf1446dd",
     "url": "./index.html"
   },
   {
-    "revision": "5749c6d7610f99578786",
-    "url": "./static/css/main.7ef3fd9d.chunk.css"
+    "revision": "000e124c942c49a14487",
+    "url": "./static/css/main.529a6886.chunk.css"
   },
   {
     "revision": "8fa8843e8c88d99aef56",
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "./static/js/2.24585e7e.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "5749c6d7610f99578786",
-    "url": "./static/js/main.dcc39451.chunk.js"
+    "revision": "000e124c942c49a14487",
+    "url": "./static/js/main.30d667d8.chunk.js"
   },
   {
     "revision": "8b3c3b96386eee1ea33a",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.7e9737f76462699262193b3e30e73c02.js"
+  "./precache-manifest.a7af3a55ed10ea1891c224ae70d1a412.js"
 );
 
 self.addEventListener('message', (event) => {
